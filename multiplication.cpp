993ccbc7bd9b6c2c1d5fcbf0dd8fc9406e6c35ed
mingchiehlin <mclin@hanyang.ac.kr>
2022-11-20 06:55:20 +0000

    add 9x9 multiplication table

---
+++ multiplication.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<10;i++){
+	    for(j=1;j<10;j++){
+        printf("%d * %d = %d \n",i, j, i*j);
+    }
+    }	    
+    cout<<"9x9 Multiplication Table! \n";
+
+ 
+
+    return 0;
+}
