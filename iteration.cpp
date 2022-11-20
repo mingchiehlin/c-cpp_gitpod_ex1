5273252ebd99c32df25bd2654000f92e81c82b55
mingchiehlin <mclin@hanyang.ac.kr>
2022-11-20 06:38:53 +0000

    add iteration.cpp

---
+++ iteration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;i<10;i++){
+        printf("%d \n",i);
+    }
+    cout<<"Hello World! \n";
+
+ 
+
+    return 0;
+}
