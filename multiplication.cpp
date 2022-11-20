4f0cde640612fe29d33c8f2162c7f3f03982557b
mingchiehlin <mclin@hanyang.ac.kr>
2022-11-20 06:59:51 +0000

    update the print out format

--- multiplication.cpp
+++ multiplication.cpp
@@ -5,8 +5,9 @@ int main()
     int i,j;
     for(i=1;i<10;i++){
 	    for(j=1;j<10;j++){
-        printf("%d * %d = %d \n",i, j, i*j);
+        printf("%d * %d = %d;",i, j, i*j);
     }
+	printf("\n");
     }	    
     cout<<"9x9 Multiplication Table! \n";
 
