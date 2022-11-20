31ed883fd2ff6c1ffb7327456146a3b094a35ec1
mingchiehlin <mclin@hanyang.ac.kr>
2022-11-20 08:00:26 +0000

    9x9 Multiplication Table formatted

---
+++ multiplication2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<10;i++){
+	    for(j=1;j<10;j++){
+        printf("%2d * %2d = %2d;",i, j, i*j);
+    }
+	printf("\n");
+    }	    
+    cout<<"9x9 Multiplication Table! \n";
+
+ 
+
+    return 0;
+}
