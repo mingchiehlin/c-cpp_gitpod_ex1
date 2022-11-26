354c92352215f33fcba772c54d2516ad2a41d5fe
mingchiehlin <mclin@hanyang.ac.kr>
2022-11-26 02:39:18 +0000

    signOfnum

---
+++ signOfnum.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+	int main (){
+	int myNum = 10; // Is this a positive or negative number?
+
+	if (myNum > 0)
+		printf("The value is a positive number. \n");
+	else if (myNum < 0)
+		printf("The value is a negative number. \n");
+	else
+		printf("The value is 0. \n");
+	return 0;
+}
+ 
